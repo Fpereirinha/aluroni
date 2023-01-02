75acb28f418cf8d26488d763b6fff8ad870ced23
Pedro Fernandes <pedrofpereira222@gmail.com>
2023-01-02 09:14:37 -0300

    novas funcionalidades

---
+++ src/pages/cardápio/buscador/index.tsx
@@ -0,0 +1,23 @@
+import { SetStateAction } from "react";
+import styles from "./buscador.module.scss";
+import { CgSearch } from "react-icons/cg";
+
+interface Props {
+	busca: string;
+	setBusca: React.Dispatch<SetStateAction<string>>;
+}
+
+export default function Buscador({ busca, setBusca }: Props) {
+	return (
+		<div className={styles.buscador}>
+			<input
+				type="text"
+				name=""
+				id=""
+				value={busca}
+				onChange={(evento) => setBusca(evento.target.value)}
+			/>
+			<CgSearch size={20} color="#4C4D5E" />
+		</div>
+	);
+}
